c0952d123be7c20a2dcc07e39d82673510566866
joelikenga <joelikenga@gmail.com>
2023-02-26 19:52:42 +0200

    search section changes

--- src/app/searchSection.js
+++ src/app/searchSection.js
@@ -1,28 +1,33 @@
-import {ArrowRight, Cart, Signout, Signin, UserIcon} from "../../public/svgIcons";
+import {ArrowRight, Cart, Signout, Signin, UserIcon, Bar} from "../../public/svgIcons";
 
 export  const SearchSection = () => {
   return(
       <main className={"sticky top-0 bg-[rgba(16,24,32,0.5)] py-4 px-6 z-50"}>
+
            {/*search input*/}
           <div className={" rounded flex justify-between"}>
 
+            {/*  sidebar*/}
+              <div className={"text-3xl text-my_dark_2 text-my_dark_2 mt-2 items-center block sm:hidden"}>
+                  <Bar className={""}/>
+              </div>
+
+
             {/*  logo name*/}
-            <div className={"my-2 w-fit block xl:hidden lg:"}>
-                <div
-                    className={"flex justify-between block xl:hidden"}>
+            <div className={"my-2 w-fit hidden sm:block xl:hidden lg:"}>
+                <div className={"flex justify-between block xl:hidden"}>
                     <h3 className={"text-xl font-extrabold text-center"}>BlackMarket</h3>
                 </div>
-
             </div>
 
 
-            <div className={"flex bg-my_light bg-my_dark rounded p-2"}>
+            <div className={"flex bg-my_light bg-my_dark rounded p-2 "}>
+
                 <input
                     type={"text"}
                     placeholder={"What do you want..."}
-                    className={
-                    " w-[ ml-3 bg-my_light bg-my_dark outline-0 font-medium text-my_dark_2 sm:min-w-fit md:min-w-fit lg:min-w-[44vw]"
-                }/>
+                    className={" ml-3 bg-my_light bg-my_dark outline-0 font-medium text-my_dark_2  sm:min-w-fit md:min-w-fit lg:min-w-[44vw]"}
+                />
 
                 <div className={"text-2xl mr-2 bg-my_dark_2 text-my_yellow px-4 py-1 rounded cursor-pointer"}>
                     <ArrowRight className={"font-black"}/>
