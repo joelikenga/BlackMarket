a8536f616a056d3172c38256c517650c089a1977
joelikenga <joelikenga@gmail.com>
2023-02-26 23:09:33 +0200

    update

--- src/app/searchSection.js
+++ src/app/searchSection.js
@@ -2,7 +2,7 @@ import {ArrowRight, Cart, Signout, Signin, UserIcon, Bar} from "../../public/svg
 
 export  const SearchSection = () => {
   return(
-      <main className={"sticky top-0 bg-[rgba(16,24,32,0.5)] py-2 sm:py-4 px-6 z-50"}>
+      <main className={"sticky top-0 bg-[rgba(16,24,32,0.5)] px-3 py-1 sm:py-4 sm:px-6 z-50"}>
 
            {/*search input*/}
           <div className={" rounded flex justify-between items-center"}>
@@ -14,7 +14,7 @@ export  const SearchSection = () => {
 
 
             {/*  logo name*/}
-            <div className={"my-2 w-fit hidden sm:block xl:hidden lg:"}>
+            <div className={"my-1 w-fit hidden sm:block xl:hidden lg:"}>
                 <div className={"flex justify-between block xl:hidden"}>
                     <h3 className={"text-xl font-extrabold text-center"}>BlackMarket</h3>
                 </div>
@@ -26,7 +26,7 @@ export  const SearchSection = () => {
                 <input
                     type={"text"}
                     placeholder={"What do you want..."}
-                    className={" h-[2rem] w-[38vw] ml-3 bg-my_light bg-my_dark outline-0 font-medium text-my_dark_2 text-sm sm:min-w-[36vw] md:w-fit md:text-xl lg:w-[44vw]"}
+                    className={" h-[1.5rem] w-[42vw] ml-3 bg-my_light bg-my_dark outline-0 font-medium text-my_dark_2 text-sm sm:h-fit sm:min-w-[36vw] md:w-fit md:text-lg lg:w-[44vw]"}
                 />
 
                 <button className={"text-xl px-2 mr-1 bg-my_dark_2 text-my_yellow rounded cursor-pointer sm:px-4 sm:text-2xl"}>
@@ -55,7 +55,7 @@ export  const SearchSection = () => {
 
                 {/*small screen account profile button*/}
                 <div className={"block md:hidden"}>
-                    <button className={" px-2 py-2 bg-my_dark_2 text-my_yellow ml-1 rounded-full inline-flex "}> <UserIcon className={"text-2xl"}/></button>
+                    <button className={" bg-my_dark_2 text-my_yellow rounded-full inline-flex "}> <UserIcon className={"text-xl m-1"}/></button>
                 </div>
             </div>
 
