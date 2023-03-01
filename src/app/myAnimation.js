b64e9e2ca2bfcfe9fbc14860ca34105801e69cdc
joelikenga <joelikenga@gmail.com>
2023-03-02 00:23:57 +0200

    update

--- src/app/myAnimation.js
+++ src/app/myAnimation.js
@@ -8,3 +8,27 @@ export const opacityVariant = {
     },
 
 }
+
+export const showSidebarVariant = {
+    start: {
+        opacity:0,
+        x:-9
+    },
+    stop: {
+        opacity:1,
+        x:-1,
+        transition: {
+            type:"spring",
+            damping:10,
+            stiffness:100,
+            delay:0.1,
+            duration: 0.2
+        }
+    }
+}
+
+export const clickVariant = {
+    click:{
+        scale:0.9
+    }
+}
